8b9cd7595482571d45e954d49df74dadac5b0014
SubhamDas-80389 <subhamdas3rd@gmail.com>
2023-09-17 17:06:14 +0530

    Assignments

---
+++ Assignment7/replace_dictionary_values.py
@@ -0,0 +1,15 @@
+student_details = [
+    {'id': 1, 'subject': 'math', 'V': 70, 'VI': 82},
+    {'id': 2, 'subject': 'math', 'V': 73, 'VI': 74},
+    {'id': 3, 'subject': 'math', 'V': 75, 'VI': 86}
+]
+
+
+for student in student_details:
+    average = (student['V'] + student['VI']) / 2
+    student['V+VI'] = average
+    del student['V']
+    del student['VI']
+
+
+print(student_details)
