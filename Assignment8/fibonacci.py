ddf27947e3157b953089032460faffbd5d6a40ed
SubhamDas-80389 <subhamdas3rd@gmail.com>
2023-09-20 02:29:28 +0530

    Assignments

---
+++ Assignment8/fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    fib_series = []
+    a, b = 0, 1
+
+    for _ in range(n):
+        fib_series.append(a)
+        a, b = b, a + b
+
+    return fib_series
+
+
+n = int(input("Enter the number of terms: "))
+fibonacci_series = fibonacci(n)
+print("Fibonacci series :", fibonacci_series)
